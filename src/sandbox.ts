dc936783962343a80d1d9c289e0b17848b9e9951
sharafath ali <sharafathaliofficial@gmail.com>
2024-12-14 22:10:07 +0530

    setup for learning typescript

---
+++ src/sandbox.ts
@@ -0,0 +1,7 @@
+var app: number = 1;
+let Firstname: string = 'Bo jjb'
+let isOdd: boolean = true;
+
+let novalue: null = null; // it cannot be assigned to any values  
+
+let nothing:  undefined = undefined; // it cannot be assigned to any value
